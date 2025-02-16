f9da5eada1124b2cc7a9cdb242b277ddc4bb7e38
Ricardo Magalhães Parente Frota <ricardoparente58@gmail.com>
2025-02-16 17:58:13 -0300

    LerDir.h

--- include/LerDir.h
+++ include/LerDir.h
@@ -2,7 +2,6 @@
 #define LERDIR_H
 
 // Declara as funcoes que serao utilizadas em LerDir.c
-void processImage(const char *fileName, int k);  
 void processDirectory(const char *directoryPath, int k);
 void clusterizarImagem(const char *imagePath, int k);
 
